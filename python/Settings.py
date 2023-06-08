4f35478da3cb275aac7c44ecbd976241d36d349a
Maurice Atrops <matrops@cologne-intelligence.de>
2023-06-08 12:09:41 +0200

    Add parser for SZ RSS feed

--- python/Settings.py
+++ python/Settings.py
@@ -1,14 +1,14 @@
 import os
 
 # Airflow configuration
-RAW_DATA_DIR = "/opt/airflow/python/data/raw"
-TRANSFORMED_DATA_DIR = "/opt/airflow/python/data/transformed"
-BASE_DIR = "/opt/airflow/python/data"
+# RAW_DATA_DIR = "/opt/airflow/python/data/raw"
+# TRANSFORMED_DATA_DIR = "/opt/airflow/python/data/transformed"
+# BASE_DIR = "/opt/airflow/python/data"
 
 # Local configuration
-# BASE_DIR = "/home/matrops/git/rss-mining/data" 
-# RAW_DATA_DIR = os.path.join(BASE_DIR, "raw")
-# TRANSFORMED_DATA_DIR = os.path.join(BASE_DIR, "transformed")
+BASE_DIR = "/home/matrops/git/rss-mining/data" 
+RAW_DATA_DIR = os.path.join(BASE_DIR, "raw")
+TRANSFORMED_DATA_DIR = os.path.join(BASE_DIR, "transformed")
 
 ZEIT_URL = "https://newsfeed.zeit.de/index"
 FAZ_URL = "https://www.faz.net/rss/aktuell"
