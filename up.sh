f347e2b1bb41b01fa87c30fb540dff3a87aacdd2
Maurice Atrops <matrops@cologne-intelligence.de>
2023-08-26 13:42:13 +0200

    Prevent container stopping if they're not running

--- up.sh
+++ up.sh
@@ -1,6 +1,8 @@
-echo "Stopping containers..."
-docker stop $(docker ps -q)
-echo "Containers stopped!"
+if [ ! -z "$(docker ps -q)" ]; then
+    echo "Stopping containers..."
+    docker stop $(docker ps -q)
+    echo "Containers stopped!"
+fi
 
 echo "Building and starting docker image"
 
